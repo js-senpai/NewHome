10a3a6ae9d8f2ada198c39dbd89874debc6f3893
karapv <andrewkarapenkov@gmail.com>
2021-03-23 23:54:06 +0200

    popups is ready

--- src/js/01__main.js
+++ src/js/01__main.js
@@ -1,3 +1,33 @@
 const lazyLoadInstance = new LazyLoad({
     // Your custom settings go here
 });
+(function ($){
+    $(document).ready(function (){
+        // Checkbox
+        if($('.form__checkbox').length){
+            $('.form__checkbox').each(function (item){
+                if(!$(this).is(':checked')){
+                    $(this).parent().parent().find('.form__checkbox-icon').addClass('not-checked')
+                    $(this).parent().parent().parent().find('.btn-action').attr('disabled')
+                }
+            })
+            $('.form__checkbox-icon').click(function (){
+               if($(this).parent().find('.form__checkbox').is(':checked')){
+                   $(this).addClass('not-checked')
+                   $(this).parent().find('.form__checkbox').removeAttr('checked')
+                   $(this).parent().parent().parent().find('.btn-action').attr('disabled')
+               } else {
+                   $(this).removeClass('not-checked')
+                   $(this).parent().find('.form__checkbox').attr('checked')
+                   $(this).parent().parent().parent().find('.btn-action').removeAttr('disabled')
+               }
+            })
+        }
+        // Popup
+        if($('.popup-overlay').length){
+            $('.popup-close').click(function (){
+                $(this).parent().parent().parent().parent().fadeOut(500)
+            })
+        }
+    })
+})(jQuery)

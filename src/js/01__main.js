040c2805cbc3059be0e7268ee4142583203222f3
karapv <andrewkarapenkov@gmail.com>
2021-03-26 01:00:53 +0200

    add new component

--- src/js/01__main.js
+++ src/js/01__main.js
@@ -3,6 +3,19 @@ const lazyLoadInstance = new LazyLoad({
 });
 (function ($){
     $(document).ready(function (){
+        //Mobile slider
+        const mobileSlider = ({mobileFunc,descFunc,mobileWidth = 880,elemClass = ''}) =>{
+            if ($(window).width() <= mobileWidth) {
+                mobileFunc()
+            }
+            $(window).resize(function () {
+                if ($(window).width() <= mobileWidth) {
+                    mobileFunc()
+                } else if ($(window).width() > mobileWidth && $(elemClass).hasClass('slick-initialized')) {
+                    descFunc();
+                }
+            });
+        }
         // Checkbox
         if($('.form__checkbox').length){
             $('.form__checkbox').each(function (item){
@@ -27,6 +40,24 @@ const lazyLoadInstance = new LazyLoad({
         if($('.popup-overlay').length){
             $('.popup-close').click(function (){
                 $(this).parent().parent().parent().parent().fadeOut(500)
+            })
+            $('.btn-open-popup-call').click(function (){
+                $('.popup-call').fadeIn(500)
+            })
+        }
+        // Stages slider
+        if($('.stages-cooperation__item').length){
+            mobileSlider({mobileFunc:()=>{$('.stages-cooperation__list').slick({
+                    arrows: true,
+                    infinite: true,
+                    speed: 300,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    lazyLoad: 'progressive',
+                    appendArrows: '.stages-cooperation__nav',
+                    prevArrow: '<span class="stages-cooperation__btn stages-cooperation__btn-left"><i class="fas fa-long-arrow-alt-left"></i></span>',
+                    nextArrow: '<span class="stages-cooperation__btn  stages-cooperation__btn-right"><i class="fas fa-long-arrow-alt-right"></i></span>',
+                })},descFunc: ()=>{$('.stages-cooperation__list').filter('.slick-initialized').slick('unslick')},mobileWidth: 768,elemClass: '.stages-cooperation__list'
             })
         }
     })
